ab969d4cec2ad83c1a271d50f40a3bd12528258f
Aaron Parker <aaron@stealthpuppy.com>
2022-08-15 10:47:35 +1000

    Re-order prep image script

--- pipeline/Install-RequiredModules.ps1
+++ pipeline/Install-RequiredModules.ps1
@@ -2,48 +2,41 @@
     .SYNOPSIS
         Installs modules required for updating the markdown and committing changes
 #>
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "", Justification="Outputs progress to the pipeline log")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "", Justification = "Outputs progress to the pipeline log")]
 [CmdletBinding()]
-param (
-    [Parameter()]
-    [System.String[]] $Modules = @("MarkdownPS", "powershell-yaml", "posh-git")
-)
+param ()
 
 #region Trust the PSGallery and install modules
-$Repository = "PSGallery"
-if (Get-PSRepository | Where-Object { $_.Name -eq $Repository -and $_.InstallationPolicy -ne "Trusted" }) {
-    try {
-        Write-Host "Trusting the repository: $Repository."
-        Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.208" -Force
-        Set-PSRepository -Name $Repository -InstallationPolicy "Trusted"
-    }
-    catch {
-        Throw $_
-        break
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.208" -Force
+Install-PackageProvider -Name "PowerShellGet" -MinimumVersion "2.2.5" -Force
+foreach ($Repository in "PSGallery") {
+    if (Get-PSRepository | Where-Object { $_.Name -eq $Repository -and $_.InstallationPolicy -ne "Trusted" }) {
+        try {
+            Write-Host "Trusting the repository: $Repository."
+            Set-PSRepository -Name $Repository -InstallationPolicy "Trusted"
+        }
+        catch {
+            $_.Exception.Message
+        }
     }
 }
 
-foreach ($module in $Modules) {
-    try {
-        Write-Host "Checking module: $module."
-        $installedModule = Get-Module -Name $module -ListAvailable | `
-            Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } | `
-            Select-Object -First 1
-        $publishedModule = Find-Module -Name $module -ErrorAction "SilentlyContinue"
-        if (($Null -eq $installedModule) -or ([System.Version]$publishedModule.Version -gt [System.Version]$installedModule.Version)) {
-            Write-Host "Installing module: $module"
-            $params = @{
-                Name               = $module
-                SkipPublisherCheck = $true
-                Force              = $true
-                ErrorAction        = "Stop"
-            }
-            Install-Module @params
+foreach ($module in @("MarkdownPS", "powershell-yaml", "posh-git")) {
+    Write-Host "Checking module: $module"
+    $installedModule = Get-Module -Name $module -ListAvailable -ErrorAction "SilentlyContinue" | `
+        Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } | `
+        Select-Object -First 1
+    $publishedModule = Find-Module -Name $module -ErrorAction "SilentlyContinue"
+    if (($Null -eq $installedModule) -or ([System.Version]$publishedModule.Version -gt [System.Version]$installedModule.Version)) {
+        Write-Host "Installing module: $module"
+        $params = @{
+            Name               = $module
+            SkipPublisherCheck = $true
+            Force              = $true
+            ErrorAction        = "Stop"
         }
-    }
-    catch {
-        Throw $_
-        break
+        Install-Module @params
     }
 }
 #endregion
